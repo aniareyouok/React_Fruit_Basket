583b0f8ad529baaaa85dd07a076f38ccfbc80a96
aniareyouok <84382283+aniareyouok@users.noreply.github.com>
2021-11-29 22:43:45 +0100

    added state to delivery frequency, moved order form to app.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, {useState} from 'react';
 import './App.css';
 import Counter from "./component/Counter";
 import Button from "./component/Button";
-import OrderForm from "./component/OrderForm";
 
 function App() {
 
@@ -17,6 +16,51 @@ function App() {
         setApple(0);
         setKiwi(0);
     }
+
+    const [state, setState] = useState({
+        name: "",
+        surname: "",
+        age: 0,
+        zip: "",
+        message: "",
+    })
+
+    function handleChange(e) {
+        const value = e.target.value;
+        setState({...state, [e.target.name]: value});
+    }
+
+    const [delivery_time, setDelivery_time] = useState(false)
+    const [frequency, setFrequency] = useState("");
+    const [checkedTerms, toggleCheckedTerms] = useState(false);
+    const [clicked, toggleClicked] = useState(false);
+
+    function handleClick() {
+        console.log(`Bestelling is verstuurd!`);
+        toggleClicked(!clicked);
+    }
+
+    function onSubmit(e) {
+        console.log(`
+            Naam: ${state.name} 
+            Achternaam: ${state.surname} 
+            Leeftijd: ${state.age} 
+            Postcode: ${state.zip} 
+            Bezorg frequentie: ${frequency} 
+            Bezorgtijd: ${delivery_time} 
+            Opmerkingen: ${state.message} 
+            Akkoord met voorwaarden: ${checkedTerms} 
+            `);
+        console.log(`
+            Fruitmand
+            Aardbeien: ${strawberry}
+            Bananen: ${banana}
+            Appels: ${apple}
+            Kiwi's: ${kiwi}
+        `)
+        e.preventDefault();
+    }
+
 
     return (
         <>
@@ -56,10 +100,109 @@ function App() {
                         type="button"
                         onClick={reset}
                         btn_message="Reset"
+                        disabled={false}
                     />
                 </div>
                 <div className="orderform">
-                    <OrderForm>Doet ie het?</OrderForm>
+                    <form onSubmit={onSubmit}>
+                        <h3>Bestel gegevens</h3>
+                        <label htmlFor="firstname_input">Voornaam:</label>
+                        <input
+                            type="text"
+                            id="firstname_input"
+                            name="name"
+                            value={state.name}
+                            onChange={handleChange}
+                        />
+                        <label htmlFor="surname_input">Achternaam:</label>
+                        <input
+                            type="text"
+                            id="surname_input"
+                            name="surname"
+                            value={state.surname}
+                            onChange={handleChange}
+                        />
+                        <label htmlFor="age_input">Leeftijd:</label>
+                        <input
+                            type="number"
+                            id="age_input"
+                            name="age"
+                            value={state.age}
+                            onChange={handleChange}
+                        />
+                        <label htmlFor="zipcode_input">Postcode:</label>
+                        <input
+                            type="text"
+                            id="zipcode_input"
+                            name="zip"
+                            value={state.zip}
+                            onChange={handleChange}
+                        />
+                        <label htmlFor="frequency_dropdown">Bezorgfrequentie</label>
+                        <select
+                            name="Frequency"
+                            id="frequency_dropdown"
+                            value={frequency}
+                            onChange={(e) => setFrequency(e.target.value)}
+                        >
+                            <option value="Make_a_choice">Maak een keuze</option>
+                            <option value="weekly">Iedere week</option>
+                            <option value="every_second_week">Elke twee weken</option>
+                            <option value="monthly">Eens per maand</option>
+                        </select>
+
+                        <span className="delivery_time_buttons">
+                <label htmlFor="daytime">Overdag</label>
+                <input
+                    type="radio"
+                    id="daytime"
+                    name="Delivery_time"
+                    value="overdag"
+                    onChange={(e) => setDelivery_time(e.target.value)}
+                />
+                <label htmlFor="evening">'sAvonds</label>
+                <input
+                    type="radio"
+                    id="evening"
+                    name="Delivery_time"
+                    value="avond"
+                    onChange={(e) => setDelivery_time(e.target.value)}
+                />
+                </span>
+
+                        <label htmlFor="message_input">Opmerkingen:</label>
+                        <textarea
+                            id="message_input"
+                            cols="27"
+                            name="message"
+                            rows="4"
+                            value={state.message}
+                            onChange={handleChange}
+                        />
+
+                        <span className="terms_of_conditions">
+                    <input
+                        type="checkbox"
+                        id="terms_of_conditions"
+                        name="Agrees_to_terms_of_conditions?"
+                        checked={checkedTerms}
+                        onChange={() => toggleCheckedTerms(!checkedTerms)}
+                    />
+                    <label htmlFor="terms_of_conditions"> Ik ga akkoord met de voorwaarden</label>
+                </span>
+                        <span className="submit_button">
+                    <Button
+                        className="submit_button"
+                        type="submit"
+                        id="submit_button"
+                        btn_message="Versturen"
+                        disabled={!checkedTerms}
+                        onClick={handleClick}
+                    />
+                </span>
+
+
+                    </form>
                 </div>
             </div>
 
--- src/component/Button.js
+++ src/component/Button.js
@@ -1,11 +1,12 @@
 import React from "react";
 
-function Button({className, type, onClick, btn_message}) {
+function Button({className, type, onClick, btn_message, disabled}) {
     return (
         <div className={className}>
             <button
                 type={type}
                 onClick={onClick}
+                disabled={disabled}
             >
                 {btn_message}
             </button>
